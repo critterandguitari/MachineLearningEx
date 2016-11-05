20ecbf01eb613a3e5e06d312c48dd97a72b631b6
Owen <owen@Owens-MacBook-Pro.local>
2016-11-05 13:56:03 -0400

    adding exercises

---
+++ machine-learning-ex1/ex1/tester.m
@@ -0,0 +1,12 @@
+%% Clear and Close Figures
+clear ; close all; clc
+
+fprintf('Loading data ...\n');
+
+%% Load Data
+data = load('ex1data2.txt');
+X = data(:, 1:2);
+y = data(:, 3);
+m = length(y);
+
+
